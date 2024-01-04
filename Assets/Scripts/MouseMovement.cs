1b6c1dc8cd654819f586323ebbc5a9139ff76437
Trần Trung Việt <137788372+trungviet2k1@users.noreply.github.com>
2024-01-05 04:41:52 +0700

    player movement

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float mouseSensitive = 100f;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitive * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitive * Time.deltaTime;
+
+        //Control rotation around x axis (Look up and down)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //Control rotation around y axis (Look up and down)
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
